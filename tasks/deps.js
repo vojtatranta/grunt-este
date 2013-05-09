ff35fcbfc8355dc1fc3620a611d592b604b2bf8f
Daniel Steigerwald <daniel@steigerwald.cz>
2013-05-10 01:33:35 +0200

    fix whitespaces

--- tasks/deps.js
+++ tasks/deps.js
@@ -42,7 +42,7 @@ module.exports = function (grunt) {
          * @type {string|Array.<string>}
          */
         root: '',
-        
+
         /**
          * Folder where is script executed
          * @type {string}
